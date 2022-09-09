887751a5345b3f902838ce4e679d4056bb24ef65
Vladimír Ulman <ulman@fi.muni.cz>
2022-09-09 15:39:25 +0200

    WIP: ZarrIoServicePlugin to use the ZarrCommandPlugin

--- src/main/java/sc/fiji/ome/zarr/fiji/ui/ZarrIoServicePlugin.java
+++ src/main/java/sc/fiji/ome/zarr/fiji/ui/ZarrIoServicePlugin.java
@@ -1,7 +1,8 @@
 package sc.fiji.ome.zarr.fiji.ui;
 
-import net.imagej.ImageJ;
+import org.scijava.command.CommandService;
 import org.scijava.log.LogService;
+import org.scijava.options.OptionsService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
@@ -9,19 +10,26 @@ import org.scijava.io.IOPlugin;
 import org.scijava.io.AbstractIOPlugin;
 import org.scijava.io.location.FileLocation;
 import org.scijava.io.location.Location;
+import org.scijava.ui.UIService;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 @Plugin(type = IOPlugin.class)
-public class DndHandler extends AbstractIOPlugin<Object> {
+public class ZarrIoServicePlugin extends AbstractIOPlugin<Object> {
 
 	@Parameter
 	private LogService logService;
 
+	@Parameter
+	private CommandService cmdService;
+
+	@Parameter
+	private OptionsService optionsService;
+
 	@Override
 	public boolean supportsOpen(Location source) {
-		logService.info("DnDHandler was questioned: "+source.getURI().getPath());
+		logService.info("ZarrIoServicePlugin was questioned: "+source.getURI().getPath());
 		if (!(source instanceof FileLocation)) return false;
 		if (!(source.getName().endsWith(".zarr"))) return false;
 		return true;
@@ -29,15 +37,16 @@ public class DndHandler extends AbstractIOPlugin<Object> {
 
 	@Override
 	public Object open(Location source) throws IOException {
-		logService.info("DnDHandler was asked to open: "+source.getURI().getPath());
+		logService.info("ZarrIoServicePlugin was asked to open: "+source.getURI().getPath());
 		final FileLocation fsource = source instanceof FileLocation ? (FileLocation)source : null;
 		if (fsource == null) return null; //NB: shouldn't happen... (in theory)
 
-		logService.info("DnDHandler is going to open: "+fsource.getFile().getAbsolutePath());
-		//
-		//we've been given a .zarr _file_, a special "tag" file inside .zarr,
-		//its parent folder is the true "handle" of this .zarr
-		new DialogAroundZarr(fsource.getFile().getParentFile().toPath());
+		logService.info("ZarrIoServicePlugin is going to open: "+fsource.getFile().getAbsolutePath());
+		//optionsService.getOptions(ZarrIoServicePlugin.class, )
+		boolean openAlwaysFirst = false; //TBA: retrieve from Fiji preferences
+		cmdService.run(ZarrCommandPlugin.class,true,
+				"zarrFolder", fsource.getFile(),
+				"openFirstItemNow", openAlwaysFirst);
 		return FAKE_INPUT;
 	}
 
@@ -47,13 +56,5 @@ public class DndHandler extends AbstractIOPlugin<Object> {
 	@Override
 	public Class<Object> getDataType() {
 		return Object.class;
-	}
-
-
-	//--------------------------------------------------------
-	public static void main(String[] args) {
-		ImageJ ij = new ImageJ();
-		ij.ui().showUI();
-		ij.ui().show("try drag-and-dropping a .zarr _file_ ....");
 	}
 }
