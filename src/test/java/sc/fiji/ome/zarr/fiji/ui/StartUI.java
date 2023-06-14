0a5cc28e28ade2d2322225c32952b6de642a69f0
Vladimír Ulman <ulman@fi.muni.cz>
2023-06-14 20:35:48 +0200

    CHG: simplified the dependency list (but not necessarily the dep tree)

--- src/test/java/sc/fiji/ome/zarr/fiji/ui/StartUI.java
+++ src/test/java/sc/fiji/ome/zarr/fiji/ui/StartUI.java
@@ -1,12 +1,10 @@
 package sc.fiji.ome.zarr.fiji.ui;
 
-import net.imagej.legacy.ui.LegacyUI;
-import org.scijava.Context;
-import org.scijava.ui.UIService;
+import net.imagej.ImageJ;
 
 public class StartUI {
 	public static void main(String[] args) {
-		final Context context = new Context();
-		context.service( UIService.class ).showUI( LegacyUI.NAME );
+		final ImageJ ij = new ImageJ();
+		ij.ui().showUI();
 	}
 }
