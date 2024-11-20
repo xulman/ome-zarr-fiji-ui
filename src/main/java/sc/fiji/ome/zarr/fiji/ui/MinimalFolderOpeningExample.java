e5c120d2aff34cc2a0c0aacf3372be83351fae1f
Vladimír Ulman <ulman@fi.muni.cz>
2024-11-20 15:42:23 +0100

    CHG: MinimalFolderOpeningExample adapted to open folder "*.xyz"  and return SelfShowableContent

--- src/main/java/sc/fiji/ome/zarr/fiji/ui/MinimalFolderOpeningExample.java
+++ src/main/java/sc/fiji/ome/zarr/fiji/ui/MinimalFolderOpeningExample.java
@@ -1,5 +1,6 @@
 package sc.fiji.ome.zarr.fiji.ui;
 
+import net.imagej.ImageJ;
 import org.scijava.io.AbstractIOPlugin;
 import org.scijava.io.IOPlugin;
 import org.scijava.io.location.Location;
@@ -9,26 +10,38 @@ import org.scijava.ui.SelfShowableContent;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Plugin(type = IOPlugin.class, attrs = @Attr(name = "eager"))
 public class MinimalFolderOpeningExample extends AbstractIOPlugin<Object> {
 	@Override
 	public boolean supportsOpen(Location source) {
-		// I only work with folders.. and steal them away
-		// from native ImageJ opener (aka Image Sequence reader)
-		return Files.isDirectory( Paths.get(source.getURI()) );
+		System.out.println(this.getClass().getName() + " is considering the file "+source);
+		//
+		final Path filePath = Paths.get(source.getURI());
+		boolean canOpen = Files.isDirectory(filePath) && filePath.toString().endsWith(".xyz");
+		System.out.println(this.getClass().getName() + " says canOpen = " + canOpen);
+		return canOpen;
 	}
 
 	@Override
 	public Object open(Location source) throws IOException {
-		final String message = this.getClass().getName() + " is opening the folder "+source;
-		System.out.println("Only a proof I was here...");
-		return new SelfShowableContent<>(message, m -> System.out.println("message was: "+m));
+		final String message = this.getClass().getName() + " is opening the file "+source;
+
+		return new SelfShowableContent<>(
+				source.toString(),
+				m -> System.out.println("I just took care of the .xyz folder: "+m)
+		);
 	}
 
 	@Override
 	public Class<Object> getDataType() {
 		return Object.class;
+	}
+
+	public static void main(String[] args) {
+		final ImageJ ij = new ImageJ();
+		ij.ui().showUI();
 	}
 }
